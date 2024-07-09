9a74820d02462d0cb9053d0274478e94d3bd1abf
steakbutter <carlosgzzperales@gmail.com>
2024-07-09 15:31:11 -0600

    added utils folder with auth.js file and code for authentication

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const withAuth = (req, res, next) => {
+    // Redirect the starting session to the Login page if you are not do a logged in
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
