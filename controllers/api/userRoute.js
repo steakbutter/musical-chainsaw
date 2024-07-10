f1b20f960838576e9ae5fff70b9df738c0a6f419
steakbutter <carlosgzzperales@gmail.com>
2024-07-09 18:09:48 -0600

    added the user login and logout route

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/user');
 
 router.post('/', async (req, res) => {
     try {
@@ -15,3 +15,47 @@ router.post('/', async (req, res) => {
         res.status(400).json(error);
     }
 });
+
+// Login Route
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: {email:req.body.email}});
+
+        if (!userData) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect email or password'});
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400)
+            .json({ message: 'Incorrect email or password'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+
+    } catch (error) {
+        res.status(400).json(err);
+    }
+});
+
+//Logout Route
+router.post('/logout', (req, res) => {
+    if( req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(200).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
