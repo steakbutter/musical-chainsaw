2bfea7730e6e2f06c5d207bf5029b4a32ade7641
steakbutter <carlosgzzperales@gmail.com>
2024-07-11 11:44:33 -0600

    Added homepage routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
 const User  = require('../models/user');
 const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: {'password'} },
+        });
+        const user = userData.get({ plain: true });
+
+        res.render('homepage', {
+            user,
+            logged_in: req.session.logged_in,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
