385a1620d46b907c4312af5b6a71d4cd7deed675
steakbutter <carlosgzzperales@gmail.com>
2024-07-13 16:49:33 -0600

    Created the Post model and associations with User model

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require('./user');
+const Post = require('./post');
+
+User.hasMany(Post, {
+    foreignKey: 'author'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'author'
+});
+
+module.exports = { User, Post };
---
+++ models/post.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true ,
+            autoIncrement: true,
+        },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        publication_date: {
+            type: DataTypes.DATE,
+        },
+        author: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post', 
+    }
+);
+
+    module.exports = Post;
