2e46273348e7779d46a9352a22dcdce6dc2ccc59
steakbutter <carlosgzzperales@gmail.com>
2024-07-17 10:18:35 -0600

    added update and delete routes to posts

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -45,3 +45,41 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(error);
     }
 });
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+    const updatePost = await Post.update({
+        title: req.body.title,
+        content: req.body.content,
+    },
+    {
+        where: { id: req.params.id},
+    }
+);
+res.status(200).json(updatePost)      
+    } catch (error) {
+        res.status(400).json(updatePost)
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const deletePost = await Post.destroy({
+            where:{
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+        if(!deletePost) {
+            res.status(400).json({ message: 'No post found with that id'});
+            return;
+        }
+        res.status(200).json(deletePost);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+});
+
+module.exports = router;
+
